cab213f8b6ce519c0522710424a513c252b59654
509Dave16 <davidfall@rocketmail.com>
2016-05-26 15:20:08 -0700

    Completed test coverage.

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,21 @@ var starWars = require('./index');
 var expect = require('chai').expect;
 
 describe('starwars-names', () => {
-  it('should work!', () => {
-    expect(true).to.be.true;
+  describe('all', () => {
+    it('should be an array of strings', () =>
+      expect(starWars.all).to.satisfy(
+        array => array.every(item => typeof item === 'string')
+      )
+    );
+
+    it('should contain `Luke Skywalker`', () =>
+      expect(starWars.all).to.include('Luke Skywalker')
+    );
+  });
+
+  describe('random', () => {
+    it('should return a random item from the starWars.all', () =>
+      expect(starWars.all).to.include(starWars.random())
+    );
   });
 });
